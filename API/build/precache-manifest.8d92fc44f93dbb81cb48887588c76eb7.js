2c03e3f32b34871cf227ab9fd5d939b814195d24
vahid <vdkkia2004@yahoo.com>
2020-02-28 11:24:31 +0100

    added build

---
+++ API/build/precache-manifest.8d92fc44f93dbb81cb48887588c76eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e83e48c0f924f0c3cb3b5711c5f4457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a9320a195f6ab729403",
+    "url": "/static/css/2.ac046694.chunk.css"
+  },
+  {
+    "revision": "ddf4efc8a7a49a71f193",
+    "url": "/static/css/main.b025ea8f.chunk.css"
+  },
+  {
+    "revision": "6a9320a195f6ab729403",
+    "url": "/static/js/2.04b2025f.chunk.js"
+  },
+  {
+    "revision": "d59d5a96bde3285f530f492e5ebbd9f7",
+    "url": "/static/js/2.04b2025f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf4efc8a7a49a71f193",
+    "url": "/static/js/main.dc041327.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
--- API/index.js
+++ API/index.js
@@ -1,4 +1,5 @@
-const data = require("./config/data")
+#!/usr/bin/env node
+const path = require('path')
 const cors = require("cors")
 const express = require("express")
 const app = express()
@@ -8,8 +9,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use('/', Router)
-
-
-app.listen(3000, () => {
+app.use(express.static(`${__dirname}/build`));
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'build/index.html')) })
+app.listen(3003, () => {
     console.log("Server started!")
 })

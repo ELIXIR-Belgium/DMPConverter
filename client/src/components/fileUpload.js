c36f14268054805aa92e536d47013ac73fc53caf
vahid kiani <vahidkiani88@gmail.com>
2020-05-19 15:58:29 +0200

    added process.env

--- client/src/components/fileUpload.js
+++ client/src/components/fileUpload.js
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
-import { FilePond } from 'react-filepond';
-import 'filepond/dist/filepond.min.css';
+import { FilePond } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+const Url = process.env.REACT_APP_Url;
 
 class FileUpload extends Component {
-    render() {
-        const { selectedFunder } = this.props
-        const server = {
-            url: `http://localhost:3000/convert/${selectedFunder}`,
-            process: {
-                onload: (res) => {
-                    var iframe = document.createElement('iframe');
-                    iframe.id = "iframe" + Math.random();
-                    iframe.style.display = 'none';
-                    document.body.appendChild(iframe);
-                    iframe.src = `http://localhost:3000/download/${res}.pdf`;
-                }
-            }
-        }
-        return (
-            <div className="uploadContainer">
-                <FilePond allowMultiple={true} server={server} />
-            </div>
-        )
-    }
+  render() {
+    const { selectedFunder } = this.props;
+    const server = {
+      url: `${Url}/convert/${selectedFunder}`,
+      process: {
+        onload: (res) => {
+          var iframe = document.createElement("iframe");
+          iframe.id = "iframe" + Math.random();
+          iframe.style.display = "none";
+          document.body.appendChild(iframe);
+          iframe.src = `${Url}/download/${res}.pdf`;
+        },
+      },
+    };
+    return (
+      <div className="uploadContainer">
+        <FilePond allowMultiple={true} server={server} />
+      </div>
+    );
+  }
 }
 
-export default FileUpload
+export default FileUpload;

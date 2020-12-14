f60dc914b3a44aad5e595039040201b3c1974cad
Vahid Kiani <vdkkia2004@yahoo.com>
2020-12-14 10:51:21 +0100

    Remove unused import

--- client/src/components/convertType.js
+++ client/src/components/convertType.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
 class ConvertType extends Component {
